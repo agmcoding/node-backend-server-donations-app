1aa3f902be8e47bed11765f5fa71389709866287
agmcoding <antonioibmachado@gmail.com>
2025-01-04 15:50:52 -0300

    Feat: add main index file

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (request, response) => {
+  response.send('Hi there!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server started. Listening on port ${PORT}...`);
+});
